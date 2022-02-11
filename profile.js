99a19f33386471ea636938502064b5fb31b74144
Habell Dolosha Purba <habelldol@gmail.com>
2022-02-11 08:56:02 +0700

    update var btnLogout1

--- profile.js
+++ profile.js
@@ -8,9 +8,9 @@ usernameData.innerHTML=dataLocalStorage.username;
 emailData.innerHTML=dataLocalStorage.email;
 passwordData.innerHTML=dataLocalStorage.password;
 
-const btnLogout = document.querySelector("#btn-logout");
+const btnLogout1 = document.querySelector("#btn-logout");
 
-btnLogout.onclick = function(){
+btnLogout1.onclick = function(){
     localStorage.setItem("isLogin",false);
 
     if("isLogin"!=true){
